eee7963ada7e72575e52f3d80d7d95e36d6d5583
shivvamm <shivam574488@gmail.com>
2023-04-05 17:01:33 +0530

    Created Schema Models

---
+++ models/transactionsModel.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const transactionSchema = new mongoose.Schema({
+  sender_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  receiver_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  amount: { type: Number, required: true },
+  status: { type: String, required: true, enum: ['Success', 'Failed'] },
+  created_at: { type: Date, default: Date.now }
+});
+
+const Transaction = mongoose.model('Transaction', transactionSchema);
+
+module.exports = Transaction;
